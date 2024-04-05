bae02664d2c2315e6db62208561310780a0c0080
Munashe Njanji <munashenjanji45@gmail.com>
2024-04-05 16:13:54 +0200

    the component, templates, data model added together with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app-todo';
+
+  private list = new TodoList("Munashe", [
+    new TodoItem("Learn flutter page navigation.", true),
+    new TodoItem("Handling data assets in flutter."),
+    new TodoItem("Testing the Flutter User Interface", true),
+    new TodoItem("Working with Firebase and Flutter"),
+    new TodoItem("Introducing Cloud Services"),
+    new TodoItem("Organizing On-Screen Data", true)
+]);
+
+get username(): string {
+    return this.list.user;
+}
+
+get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+}
+
+get items(): readonly TodoItem[] {
+    return this.list.items
+        .filter(item => this.showComplete || !item.complete);
+}
+
+addItem(newItem: string) {
+    if (newItem != "") {
+        this.list.addItem(newItem);
+    }
+}
+
+showComplete: boolean = false;
 }
